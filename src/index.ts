0a72b8c2d3224c87baa300599e7c2cf8b67df863
Kathy Luo <kathyluo18@gmail.com>
2023-06-28 11:54:34 +0200

    chore: update api key

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 // import axios from 'axios'
 
-// const GOOGLE_API_KEY = 'AIzaSyDjQFQlKgagHtInQczOUh4Yor7RO4i8tmE'
+// const GOOGLE_API_KEY = 'AIzaSyAY5j8jNfrrHJsjEW3QCPgezFgIO0auXeQ'
 
 // @ts-ignore - travelLog is not used yet
 export async function main(travelLog: string[]) {
