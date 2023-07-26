c2904dc7b3608cdb2d97190274b3d4555e55c025
Yuri Avdijevski <dtoxvanilla@gmail.com>
2023-07-26 10:00:32 -0500

    fix: adding minor patches

--- src/index.ts
+++ src/index.ts
@@ -7,38 +7,36 @@ export async function main(travelLog: string[]): Promise<number> {
   let totalDistance = 0
   let index = 0
   //basically logic I went over at the end of the interview
-  //looping until there is no more of next index
-  while (travelLog[index + 1] !== undefined) {
-    // i got rid of the both consts and just put both origin and destination in the url, i dont think it worsens much the readability
-    const url = `https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${
-      travelLog[index]
-    }&destinations=${travelLog[index + 1]}&key=${GOOGLE_API_KEY}`
-    //getting the raw data from the api
-    const rawDistance = await axios(url)
-    //adding little grace in case the api fails
-    if (rawDistance.data.status !== 'OK') {
-      throw new Error('Google API Error, check your API key or your input')
-    } else if (
-      typeof rawDistance.data.rows[0].elements[0].distance.text !== 'string'
-    ) {
-      throw new Error(
-        'Google API Error, location not found, check your coordinates',
-      )
-    }
-    //getting the distance in miles and converting it to a number
-    const miles = +rawDistance.data.rows[0].elements[0].distance.text.substring(
-      0,
-      rawDistance.data.rows[0].elements[0].distance.text.length - 3,
-    )
-    //converting miles to meters
-    const meters = (miles * 1609.34).toFixed(2)
-    //adding up each interation of the loop
-    totalDistance += Number(meters)
+  try{
+    //looping until there is no more of next index
+    while (travelLog[index + 1] !== undefined) {
 
-    // just adding index to avoid infinite loop and properly loop through the array
-    index++
+      // i got rid of the both consts and just put both origin and destination in the url, i dont think it worsens much the readability
+      const url = `https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${travelLog[index]}&destinations=${travelLog[index+1]}&key=${GOOGLE_API_KEY}`
+      //getting the raw data from the api
+      const rawDistance = await axios(url)
+      //adding little grace in case the api fails
+      if (rawDistance.data.status !== 'OK') {
+        throw new Error(`Google API Error, check your API key or your input, status error: ${rawDistance.status}`)
+      }
+      //getting the distance in miles and converting it to a number
+      const miles = rawDistance.data.rows[0].elements[0].distance.text.substring(
+          0,
+          rawDistance.data.rows[0].elements[0].distance.text.length - 3,
+      )
+      const properFormatMeters = miles.replace(/,/, '.');
+      //converting miles to meters
+      const meters = (Number(properFormatMeters) * 1609.34).toFixed(2)
+      //adding up each interation of the loop
+      totalDistance += Number(meters)
+
+      // just adding index to avoid infinite loop and properly loop through the array
+      index++
+    }
+    return totalDistance
+  }catch(err){
+    console.log(err)
   }
-  // console.log(totalDistance)
-  return totalDistance
 }
-main(['New York, NY, USA', 'San Francisco, CA, USA'])
+
+main(["New York, NY, USA",  "35.389,-79.1455",  "San Francisco, CA, USA",  "18.4796,-89.3848"])
